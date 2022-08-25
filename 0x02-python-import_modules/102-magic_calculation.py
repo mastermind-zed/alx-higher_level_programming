646db1e5ed0a55baab792d396cf3c4c728a8a921
mastermind-zed <niiadjei0169@gmail.com>
2022-08-25 08:39:29 -0700

    ByteCode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School"""
     from magic_calculation_102 import add, sub
-    if x < y:
-        n = add(x, y)
-        for m in range(4, 6):
-            n = add(n, m)
-        return (n)
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+        return (c)
     else:
-        return(sub(x, y))
+        return (sub(a, b))
