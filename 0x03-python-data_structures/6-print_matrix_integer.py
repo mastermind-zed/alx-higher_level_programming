aeeb33635299c4a73bd8ccc01656f4084039c17b
mastermind-zed <niiadjei0169@gmail.com>
2022-08-27 02:35:14 -0700

    foxed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers"""
 
     for a in range(len(matrix)):
-        subm_len = len(matrix[i])
+        subm_len = len(matrix[a])
         for b in range(subm_len):
             if b != subm_len - 1:
                 endCh = ' '
