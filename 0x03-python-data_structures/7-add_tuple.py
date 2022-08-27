f4aa1cb6cabbfb642dcaa618d8e02a48fa9a2e4a
mastermind-zed <niiadjei0169@gmail.com>
2022-08-27 03:28:29 -0700

    fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    """
-    adds the first two elements of two tuples together
-    and returns the result
-    """
-    
-    if (len(tuple_a) == 0):
-        tuple_a = (0, 0)
-    elif (len(tuple_a) == 1):
-        tuple_a = (tuple_a[0], 0)
-    
-    if (len(tuple_b) == 0):
-        tuple_b = (0, 0)
-    elif (len(tuple_b) == 1):
-        tuple_b = (tuple_b[0], 0)
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tuple_a = tuple_a[0], 0
+        else:
+            tuple_a = 0, 0
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            tuple_b = tuple_b[0], 0
+        else:
+            tuple_b = 0, 0
+
+    ans = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (ans)
