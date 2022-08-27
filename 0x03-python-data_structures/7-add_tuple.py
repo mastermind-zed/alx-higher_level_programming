b5b5942f257ef444fa7bfaf6872c4e45b3c27640
mastermind-zed <niiadjei0169@gmail.com>
2022-08-27 03:05:04 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """ function that adds 2 tuples """
-
+    """ function that add tuples """
+    
     ta_len = len(tuple_a)
     tb_len = len(tuple_b)
     new_tup = ()
     for x in range(2):
         if x >= ta_len:
-            val_n = 0
+            val_a = 0
         else:
-            val_n = tuple_a[x]
-           
-           if x >= tb_len:
-            val_m = 0
+            val_a = tuple_a[x]
+        
+        if x >= tb_len:
+            val_b = 0
         else:
-            val_m = tuple_b[x]
+            val_b = tuple_b[x]
 
         if (x == 0):
-            new_tup = (val_n + val_m)
+            new_tup = (val_a + val_b)
         else:
-            new_tup = (new_tup, val_n + val_m)
-
+            new_tup = (new_tup, val_a + val_b)
     return (new_tup)
