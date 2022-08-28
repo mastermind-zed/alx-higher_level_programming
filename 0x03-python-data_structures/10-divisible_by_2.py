646b35b2509c6d063437b6a87e945636b37bea65
mastermind-zed <niiadjei0169@gmail.com>
2022-08-28 05:42:59 -0700

    fixed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list=[]):
+def max_integer(my_list=[]):
     """ function that finds all multiples of 2 in a list """
 
-    if len(my_list) == 0:
-        return (None)
-    my_list.sort()
-    return (my_list[-1])
+    new_list = []
+    for number in my_list:
+        if (number % 2) == 0:
+            new_list.append(True)
+
+        else:
+            new_list.append(False)
+    
+    return(new_list)
