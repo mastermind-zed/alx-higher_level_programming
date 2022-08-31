0272e53b4798d2935c144853bfc36b7e73039c91
mastermind-zed <niiadjei0169@gmail.com>
2022-08-31 14:51:21 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    """ function that replaces all occurrences of an element by another in a new list """
-    return (sum({ele for ele in my_list}))
+    res = 0
+    for value in set(my_list):
+        res += value
+    return (res)
