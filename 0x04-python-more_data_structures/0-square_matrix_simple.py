3290676c2317aafce6faddbe6233c005df09a931
mastermind-zed <niiadjei0169@gmail.com>
2022-08-31 13:25:02 -0700

    fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    """ function that computes the square value of all integers of a matrix """
-    
-    return ([[(i**2) for i in row] for row in matrix])
+    new_matrix = [[j ** 2 for j in i] for i in matrix]
+    return (new_matrix)
