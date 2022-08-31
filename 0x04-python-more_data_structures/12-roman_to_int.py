b86d1672bd19b061acf887ab6950a5bbb9ebe50b
mastermind-zed <niiadjei0169@gmail.com>
2022-08-31 15:02:05 -0700

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (roman_string is None) or (type(roman_string) != str):
-            return 0
-
-    roman = 0
-    nums = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    limit = len(roman_string)
-    for ch in range(0, limit):
-        if ch == len(roman_string) - 1:
-            roman += nums[roman_string[ch]]
-        elif nums[roman_string[ch]] >= nums[roman_string[ch + 1]]:
-            roman += nums[roman_string[ch]]
-        else:
-            roman -= nums[roman_string[ch]]
-    return roman
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    numeral = {'I': 1, 'V': 5, 'X': 10, 'IV': -1, 'IX': -1, 'L': 50, 'C': 100,
+               'D': 500, 'M': 1000, 'CM': -100, 'XC': -10}
+    num = []
+    roman_string += 'z'
+    for first, second in zip(roman_string, roman_string[1:]):
+        try:
+            num.append(numeral[first + second])
+        except:
+            num.append(numeral.get(first))
+    return sum(num)
