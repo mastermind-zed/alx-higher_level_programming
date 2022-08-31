497fa7c904dda815cca21ec74d5473987389d6fb
mastermind-zed <niiadjei0169@gmail.com>
2022-08-31 14:57:24 -0700

    checked

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,53 +1,25 @@
 #!/usr/bin/python3
-
-def convert_roman(ch):
-    ret = -1
-    if ch == 'I':
-        ret = 1
-    elif ch == 'V':
-        ret = 5
-    elif ch == 'X':
-        ret = 10
-    elif ch == 'L':
-        ret = 50
-    elif ch == 'C':
-        ret = 100
-    elif ch == 'D':
-        ret = 500
-    elif ch == 'M':
-        ret = 1000
-    return ret
-
 def roman_to_int(roman_string):
-    cur_max = -1
-    cur = conv = 0
-    holder = []
-
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    for c in roman_string:
-        cur = convert_roman(c)
-        if cur == -1:
+    if (roman_string is None) or (type(roman_string) != str):
             return 0
-        if len(holder) == 0:
-            if cur == cur_max or cur_max == -1:
-                cur_max = cur
-                conv += cur
-            elif cur < cur_max:
-                holder.append(cur)
-            elif cur > cur_max:
-                cur_max = cur
-                cur -= conv
-                conv = cur
-        else:
-            if cur > holder[-1]:
-                cur_max = cur
-                cur -= sum(holder)
-                conv += cur
-                holder.clear()
-            else:
-                holder.append(cur)
 
-    if len(holder) != 0:
-        conv += sum(holder)
-    return conv
+    roman = 0
+    nums = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    limit = len(roman_string)
+    for ch in range(0, limit):
+        if ch == len(roman_string) - 1:
+            roman += nums[roman_string[ch]]
+        elif nums[roman_string[ch]] >= nums[roman_string[ch + 1]]:
+            roman += nums[roman_string[ch]]
+        else:
+            roman -= nums[roman_string[ch]]
+    return roman
