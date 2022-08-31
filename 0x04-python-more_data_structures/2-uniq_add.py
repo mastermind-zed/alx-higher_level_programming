2c8fdf9aff0b24fc71af24af4c1bae95730459e0
mastermind-zed <niiadjei0169@gmail.com>
2022-08-31 14:10:39 -0700

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """ function that replaces all occurrences of an element by another in a new list """
+    
+    return (sum({ele for ele in my_list}))
