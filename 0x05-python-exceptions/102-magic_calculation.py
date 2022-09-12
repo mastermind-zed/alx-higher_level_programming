b5b47a65e7fb3a0e13e5ac76fae1c2b22bc6550a
mastermind-zed <niiadjei0169@gmail.com>
2022-09-12 06:46:43 -0700

    102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,13 +3,13 @@
 def magic_calculation(a, b):
     """ Python function def magic_calculation(a, b): that does exactly the same as the following """
 
-    result = 0
-    for i in range(1, 3):
+    ans = 0
+    for y in range(1, 3):
         try:
-            if i > a:
+            if y > a:
                 raise Exception('Too far')
-            result += a ** b / i
+            ans += a ** b / y
         except Exception:
-            result = b + a
+            ans = b + a
             break
-    return result
+    return (ans)
