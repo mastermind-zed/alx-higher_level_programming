b317383369bbdef4506da72967d36dbfb9db201d
mastermind-zed <niiadjei0169@gmail.com>
2022-09-13 16:31:51 -0700

    area of sqaure

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,6 +6,11 @@ class Square:
     """ defining a sqaure"""
 
     def __init__(self, size=0):
+
+        """ initializating square
+            Args:
+                size (int): size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
