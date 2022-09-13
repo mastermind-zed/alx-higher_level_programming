b4ab62b0021fdff53ad773faff93055e49609275
mastermind-zed <niiadjei0169@gmail.com>
2022-09-13 15:56:14 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class Square that defines a square by: (based on 0-square.py) """
+
+class Square:
+    """ representing a sqaure """
+
+    def __init__(self, size):
+
+        """ intitializing a new square
+            Args:
+                size (no type/value verification): size of the square.
+        """
+        self.__size = size
