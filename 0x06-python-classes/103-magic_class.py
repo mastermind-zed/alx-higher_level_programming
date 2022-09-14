a1ee0a26fe5fef866a3fc934bfbe8827c6fcf4c2
mastermind-zed <niiadjei0169@gmail.com>
2022-09-13 18:16:38 -0700

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,22 +6,19 @@ import math
 
 
 class MagicClass:
-    """Represent a circle."""
+    """ circle """
 
     def __init__(self, radius=0):
-        """Initialize a MagicClass.
-        Arg:
-            radius (float or int): The radius of the new MagicClass.
-        """
+        """Disassembly of __init__"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
-    def area(self):
-        """Return the area of the MagicClass."""
-        return (self.__radius ** 2 * math.pi)
-
     def circumference(self):
-        """Return The circumference of the MagicClass."""
+        """Disassembly of circumference"""
         return (2 * math.pi * self.__radius)
+
+    def area(self):
+        """Disassembly of area"""
+        return (self.__radius ** 2 * math.pi)
