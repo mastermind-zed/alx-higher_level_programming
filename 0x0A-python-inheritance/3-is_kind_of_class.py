af8ac7a362aef421b4ed200b994a14b61d0aa9df
mastermind-zed <niiadjei0169@gmail.com>
2022-09-26 15:01:45 -0700

    3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -12,4 +12,8 @@ def is_kind_of_class(obj, a_class):
             a_class (class): class.
         Return: True or false.
     """
-    return isinstance(obj, a_class)
+    if not isinstance(a_class, type):
+        raise TypeError("a_class type must be 'type'")
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return True
+    return False
