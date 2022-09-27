f0c59f69ae6d69ebc53756471cafd1c5d93f52cd
mastermind-zed <niiadjei0169@gmail.com>
2022-09-26 17:31:12 -0700

    imp

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  Improve Geometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry = __import__('6-base_geometry').BaseGeometry """
+    def area(self):
+        """ a class BaseGeometry (based on 5-base_geometry.py) """
+        raise Exception("area() is not implemented")
