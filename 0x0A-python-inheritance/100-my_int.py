ffba873de52671f2afcd5c7b4c9afb57987e636f
mastermind-zed <niiadjei0169@gmail.com>
2022-09-26 18:41:41 -0700

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" my int """
+
+
+class MyInt(int):
+    """ class MyInt that inherits from int """ 
+    def __init__(self, value):
+        self.num = value
+
+    def __eq__(self, other):
+        return self.num != other
+
+    def __ne__(self, other):
+        return self.num == other
