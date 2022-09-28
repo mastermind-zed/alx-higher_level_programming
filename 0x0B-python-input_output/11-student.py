72b9a5f88ecd8a65c8bef716644842144ea1c11f
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 12:21:09 -0700

    added

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,26 +2,20 @@
 """ Student to disk and reload """
 
 
-class Student: 
+class Student:
     """ class Student that defines a student by: (based on 10-student.py) """
-    """
-        A class students that defines a student by:
-        Attributes:
-            first_name (str): name of student.
-            last_name (str): name of student.
-            age (int): age of student.
-        Methods:
-            __init__ - initializes the Student instance.
-            to_json - retrieves dictionary repr of Student instance.
-    """
     def __init__(self, first_name, last_name, age):
-        """ student instance  initialization """
+        """ student instance  initialization  """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """ Gets a dictionary representation of the Student """
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+                attr (list): attribute names that are to be retrieved.
+        """
 
         if attr is not None:
             res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
@@ -30,6 +24,10 @@ class Student:
             return self.__dict__
 
     def reload_from_json(self, json):
-        """ Replaces all attributes of the Student """
-        for k, v in json.items():
-            setattr(self, k, v)
+        """
+            replaces all attributes of the Student Instance.
+            Args:
+                json (dictionary): reload data.
+        """
+        for key, value in json.items():
+            self.__setattr__(key, value)
