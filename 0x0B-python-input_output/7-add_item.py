ec1aa51ede4e5e86f35c6de969a25ff4a40ce880
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 11:20:04 -0700

    added

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-open("add_item.json", "a")
-try:
-    i = load("add_item.json")
-except ValueError:
-    i = []
-save(i + sys.argv[1:], "add_item.json")
+if __name__ == "__main__":
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
