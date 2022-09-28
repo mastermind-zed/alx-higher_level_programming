f169c2eb2df3f89e14527309d4b38a750032126f
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 10:58:54 -0700

    added

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Write to a file """
-
-
-def write_file(filename="", text=""):
-    """string to a text file (UTF8) and returns the number of characters written"""
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
