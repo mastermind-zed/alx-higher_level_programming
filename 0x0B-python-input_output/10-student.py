214376bfa9dcdfa0ec610b81e260bf3d1066d8c1
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 12:05:51 -0700

    added

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,17 +4,26 @@
 
 class Student:
     """ class Student that defines a student by: (based on 9-student.py) """
+     
      def __init__(self, first_name, last_name, age):
-         """ student instance  initialization """
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
-        """ retrieves a dictionary representation of Student. """
-        
-        if attr is not None:
-            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
-            return res
-        else:
-            return self.__dict__
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
