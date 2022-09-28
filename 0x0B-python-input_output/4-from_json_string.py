2328a06dc4cfddcb664ddc68476c7426417adaf2
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 11:07:48 -0700

    added

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,5 +5,5 @@ import json
 
 
 def from_json_string(my_str):
-    """ function that returns an object (Python data structure) represented by a JSON string """
+    """  JSON string  to object """
     return json.loads(my_str)
