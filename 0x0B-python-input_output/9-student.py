fa2dec3e25776862d2e41cd7c3630dddfff943f1
mastermind-zed <niiadjei0169@gmail.com>
2022-09-28 11:51:04 -0700

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pytho3
+""" Student to JSON """
+
+
+class Student:
+    """ Student that defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ student instance  initialization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of Student. """
+        return self.__dict__
