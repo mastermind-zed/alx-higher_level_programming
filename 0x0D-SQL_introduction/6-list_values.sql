3b943c83d327075a23d0f1e1f452f9cc1d2b3da8
mastermind-zed <niiadjei0169@gmail.com>
2022-11-01 03:51:41 -0700

    fixed

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
+SELECT * FROM first_table;
