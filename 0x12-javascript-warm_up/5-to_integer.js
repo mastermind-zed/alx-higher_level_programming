1a45aa4f109ac24721c3fa608cdcc9ea155c1e4e
mastermind-zed <niiadjei0169@gmial.com>
2022-11-28 23:05:46 +0000

    fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  for (let y = 0; y < x; y++) {
-    theFunction();
+
+const process = require('process');
+let resp;
+let num;
+resp = 'Not a number';
+if (process.argv.length > 2) {
+  num = parseInt(process.argv[2]);
+  if (!isNaN(num)) {
+    resp = 'My number: ' + String(num);
   }
-};
+}
+console.log(resp);
