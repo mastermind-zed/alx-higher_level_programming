49a3a9b73d1e01ee4f79c69ae697be6d3c931629
mastermind-zed <niiadjei0169@gmial.com>
2022-11-29 12:47:46 +0000

    added

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-module.exports = class rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-     this.width = w;
-     this.height = h;
-   }
+    this.width = w;
+    this.height = h;
+  }
 };
