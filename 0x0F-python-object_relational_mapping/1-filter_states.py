c6d4bd43d6770204c9673f14a9172f1e1f4d50f4
mastermind-zed <niiadjei0169@gmail.com>
2022-12-03 05:59:14 -0800

    added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" A script that lists all states from the database """
+
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
+"""
 
 
 import sys
