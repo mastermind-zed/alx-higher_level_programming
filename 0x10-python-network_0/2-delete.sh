ba7a11c635537d4ad5159e4626ffd1997fb2eb7f
mastermind-zed <niiadjei0169@gmail.com>
2022-12-15 14:41:33 -0800

    fixed

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE "$1"
