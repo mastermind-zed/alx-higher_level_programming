8953ba85c24f8768b83e7e6a58c810b62c969c75
mastermind-zed <niiadjei0169@gmail.com>
2022-12-16 04:16:34 -0800

    added

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+    Entry Point 
+"""
+from requests import get
+
+if __name__ == '__main__':
+    res = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(res.text))
+    print('\t- content:', res.text)
