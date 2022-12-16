e3d2efd40ea0e051655c188c3097c7d753c10cdb
mastermind-zed <niiadjei0169@gmail.com>
2022-12-16 04:41:31 -0800

    added

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" 
+"""
     Entry Point
 """
-import sys
+from sys import argv
+from requests import get
 
-import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    request = get(argv[1])
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
     else:
-        print(r.text)
+        print(request.text)
