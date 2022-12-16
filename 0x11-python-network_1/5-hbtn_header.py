8a9e829a3b628e6fea3e33cd6295c7be281696d2
mastermind-zed <niiadjei0169@gmail.com>
2022-12-16 04:30:45 -0800

    added

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-    Entry Point 
+    Entry Point
 """
+from sys import argv
+from requests import get
 
-import requests
-import sys
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    print(r.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = argv[1]
+    r = get(url)
+    print(r.headers.get("X-Request-Id"))
